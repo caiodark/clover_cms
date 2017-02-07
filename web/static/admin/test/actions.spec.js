d0bfe9c347d78c015bf87c64c69dc107752584d8
Claudio Brogliato <cbrogliato@gmail.com>
2017-02-07 09:31:47 +0100

    tested action creation

--- web/static/admin/test/actions.spec.js
+++ web/static/admin/test/actions.spec.js
@@ -12,4 +12,22 @@ describe('actions', () => {
     }
     expect(actions.login(username, password)).toEqual(expected)
   })
+  it ('should create an action to Log out', () => {
+    const expected = {
+      type: types.LOGOUT
+    }
+    expect(actions.logout()).toEqual(expected)
+  })
+  it ('should create an action to toggle the drawer', () => {
+    const expected = {
+      type: types.TOGGLE_DRAWER
+    }
+    expect(actions.toggle_drawer()).toEqual(expected)
+  })
+  it ('should create an action to go to dashboard', () => {
+    const expected = {
+      type: types.GOTO_DASHBOARD
+    }
+    expect(actions.goto_dashboard()).toEqual(expected)
+  })
 })
